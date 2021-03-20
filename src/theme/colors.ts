1ad393ca4506885ffa46959bac2e85a4380d7453
David Armendariz <darmendariz1998@outlook.com>
2021-03-20 16:07:36 -0500

    Created app colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,15 @@
+export const commonColors = {
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export const appColors = {
+  background: commonColors.white,
+  font: commonColors.black,
+  primary: '#203040',
+};
+
+export const darkModeColors = {
+  font: commonColors.white,
+  background: '#232B32',
+};
