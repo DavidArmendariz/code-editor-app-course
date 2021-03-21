7daf475273069c975c85a8c0b773af6fd024819a
David Armendariz <darmendariz1998@outlook.com>
2021-03-21 13:59:42 -0500

    Corrected some imports

--- src/store/reducers/files/reducer.ts
+++ src/store/reducers/files/reducer.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import UserFile from '../../types/UserFile';
+import UserFile from '../../../types/UserFile';
 
 export interface FilesState {
   userFiles: UserFile[];
