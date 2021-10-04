69127318a7be1af398c7dab8ca9748e80b51f3cb
David Armendariz <darmendariz1998@outlook.com>
2021-10-04 13:10:11 -0500

    Fixed issue with MUI theme

--- src/theme/CustomThemeProvider.tsx
+++ src/theme/CustomThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { createTheme, ThemeProvider } from '@mui/system';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { PropsWithChildren } from 'react';
 import { useAppSelector } from 'store/hooks';
 import { appColors, darkModeColors, commonColors } from './colors';
