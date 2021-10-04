701b1f90041696397fdac0ca660c177f75bb48aa
David Armendariz <darmendariz1998@outlook.com>
2021-10-04 18:13:56 -0500

    Simplified config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,4 @@
-const appConfig = {
+export default {
   auth0Domain: process.env.REACT_APP_AUTH0_DOMAIN || '',
   auth0ClientId: process.env.REACT_APP_AUTH0_CLIENT_ID || '',
 };
-
-export default appConfig;
