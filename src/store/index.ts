61bc63c94fa05b2100bfdc369dd59686e7e46d46
David Armendariz <darmendariz1998@outlook.com>
2021-10-05 18:19:08 -0500

    Fixed default middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 import persistStore from 'redux-persist/es/persistStore';
 import persistedReducer from './persistConfig';
@@ -7,11 +7,12 @@ const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
 
 const store = configureStore({
   reducer: persistedReducer,
-  middleware: getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions,
-    },
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions,
+      },
+    }),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
