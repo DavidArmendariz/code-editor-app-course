be545241c0b004cb3255b3510c68a15bbcab8d6b
David Armendariz <darmendariz1998@outlook.com>
2021-10-08 21:31:13 -0500

    Renamed rootReducer.ts to root-reducer.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 import persistStore from 'redux-persist/es/persistStore';
-import rootReducer from './rootReducer';
+import rootReducer from './root-reducer';
 
 const ignoredActions = [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER];
 
