a5b44add69850e046b0c8acd18a12fe5e2b51709
David Armendariz <darmendariz1998@outlook.com>
2021-10-09 18:51:12 -0500

    Fixed select active files

--- src/store/selectors/select-active-files/selectActiveFiles.ts
+++ src/store/selectors/select-active-files/selectActiveFiles.ts
@@ -1,10 +1,28 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { FilesState } from 'store/slices/files/files';
 import { RootState } from 'types/Store';
+import UserFile from 'types/UserFile';
+
+type ActiveFilesIdsMap = { [key: string]: string };
+type ActiveFilesMap = { [key: string]: UserFile };
 
 const selectActiveFiles = (files: FilesState) => {
   const { userFiles, activeFilesIds } = files;
-  return userFiles.filter((file) => activeFilesIds.includes(file.id));
+
+  const activeFilesIdsMap = activeFilesIds.reduce((result, activeFile) => {
+    result[activeFile] = activeFile;
+    return result;
+  }, {} as ActiveFilesIdsMap);
+
+  const activeFiles = userFiles
+    .filter((file) => activeFilesIdsMap[file.id])
+    .reduce((result, activeFile) => {
+      const { id } = activeFile;
+      result[id] = activeFile;
+      return result;
+    }, {} as ActiveFilesMap);
+
+  return activeFilesIds.map((activeFileId) => activeFiles[activeFileId]);
 };
 
 export default createSelector((state: RootState) => state.files, selectActiveFiles);
