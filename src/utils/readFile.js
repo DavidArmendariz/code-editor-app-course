9b40fbfa1af1859c913f22733ac2f7e1efc7ebd3
David Armendariz <darmendariz1998@outlook.com>
2021-10-10 17:29:51 -0500

    changed readFile to javascript

--- src/utils/readFile.js
+++ src/utils/readFile.js
@@ -1,8 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
-import { CustomFile } from 'types/CustomFile';
-import UserFile from 'types/UserFile';
 
-export const readSingleFile = (file: CustomFile): Promise<UserFile> => {
+export const readSingleFile = (file) => {
   return new Promise((resolve) => {
     const reader = new FileReader();
     reader.readAsText(file);
