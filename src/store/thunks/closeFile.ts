4c83499804b73d50f237b851cbd8df9f0bb4c49b
David Armendariz <darmendariz1998@outlook.com>
2021-10-10 18:32:00 -0500

    Refactored closeFile thunk

--- src/store/thunks/closeFile.ts
+++ src/store/thunks/closeFile.ts
@@ -1,27 +1,28 @@
 import { removeActiveFile, setEditorActiveFile } from 'store/slices/files/files';
 import { AppDispatch, RootState } from 'types/Store';
 
+const closeFile = (fileToCloseId: string) => (dispatch: AppDispatch, getState: () => RootState) => {
+  const {
+    files: { activeFilesIds, editorActiveFile },
+  } = getState();
+  const activeFilesLength = activeFilesIds.length;
+
+  if (activeFilesLength <= 1) {
+    dispatch(setEditorActiveFile(null));
+  } else if (activeFilesLength >= 2 && fileToCloseId === editorActiveFile) {
+    const newActiveFileId = getNewActiveFileId(activeFilesIds, activeFilesLength, fileToCloseId);
+    dispatch(setEditorActiveFile(newActiveFileId));
+  }
+
+  dispatch(removeActiveFile(fileToCloseId));
+};
+
 const getNewActiveFileId = (activeFilesIds: string[], activeFilesLength: number, fileId: string) => {
   const fileToBeRemovedIndex = activeFilesIds.indexOf(fileId);
   if (fileToBeRemovedIndex + 1 === activeFilesLength) {
     return activeFilesIds[fileToBeRemovedIndex - 1];
   }
   return activeFilesIds[fileToBeRemovedIndex + 1];
-};
-
-const closeFile = (fileToCloseId: string) => (dispatch: AppDispatch, getState: () => RootState) => {
-  const state = getState();
-  const { activeFilesIds, editorActiveFile } = state.files;
-  const activeFilesLength = activeFilesIds.length;
-
-  if (activeFilesLength >= 2 && fileToCloseId === editorActiveFile) {
-    const newActiveFileId = getNewActiveFileId(activeFilesIds, activeFilesLength, fileToCloseId);
-    dispatch(setEditorActiveFile(newActiveFileId));
-  } else if (activeFilesLength <= 1) {
-    dispatch(setEditorActiveFile(null));
-  }
-
-  dispatch(removeActiveFile(fileToCloseId));
 };
 
 export default closeFile;
