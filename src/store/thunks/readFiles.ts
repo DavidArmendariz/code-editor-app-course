c4863353250d505992cc1097aa3519172e42babf
David Armendariz <darmendariz1998@outlook.com>
2021-10-10 20:57:07 -0500

    Simplified readFiles

--- src/store/thunks/readFiles.ts
+++ src/store/thunks/readFiles.ts
@@ -3,11 +3,13 @@ import { setFiles } from 'store/slices/files/files';
 import { readSingleFile } from 'utils/readFile';
 
 const readFiles = createAsyncThunk('files/readFiles', async (files: FileList, { dispatch }) => {
-  const numberOfFiles = files.length;
-  const promises = Array.from({ length: numberOfFiles }, (_, i) => {
+  const promises = [];
+
+  for (let i = 0; i < files.length; i++) {
     const file = files[i];
-    return readSingleFile(file);
-  });
+    promises.push(readSingleFile(file));
+  }
+
   const userFiles = await Promise.all(promises);
   dispatch(setFiles(userFiles));
 });
