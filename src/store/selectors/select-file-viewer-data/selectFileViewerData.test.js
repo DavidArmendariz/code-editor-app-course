de22b260ee041f2ab8debcfb723278b35666d557
David Armendariz <darmendariz1998@outlook.com>
2021-10-11 09:56:42 -0500

    Added a more robust test for selectFileViewerData

--- src/store/selectors/select-file-viewer-data/selectFileViewerData.test.js
+++ src/store/selectors/select-file-viewer-data/selectFileViewerData.test.js
@@ -4,31 +4,39 @@ test('should convert a list of user files to the file viewer structure', () => {
   const userFiles = [
     {
       id: '1',
-      name: 'index1.js',
-      relativePath: 'test/index1.js',
-      code: 'console.log("Hello world!")',
-      extension: 'js',
+      name: 'index1.jsx',
+      relativePath: 'test/subfolder/another-subfolder/index1.jsx',
+      extension: 'jsx',
     },
     {
       id: '2',
       name: 'index2.js',
       relativePath: 'test/subfolder/index2.js',
-      code: 'console.log("Hello world!")',
       extension: 'js',
     },
     {
       id: '3',
       name: 'index3.js',
-      relativePath: 'test/index3.js',
-      code: 'console.log("Hello world!")',
+      relativePath: 'test/subfolder2/index3.js',
       extension: 'js',
     },
     {
       id: '4',
-      name: 'index4.js',
-      relativePath: 'test/subfolder/another-subfolder/index4.js',
-      code: 'console.log("Hello world!")',
+      name: 'index4.ts',
+      relativePath: 'test/subfolder3/another-subfolder/index4.ts',
+      extension: 'ts',
+    },
+    {
+      id: '5',
+      name: 'index5.js',
+      relativePath: 'test/index5.js',
       extension: 'js',
+    },
+    {
+      id: '6',
+      name: 'index6.vue',
+      relativePath: 'test/index6.vue',
+      extension: 'vue',
     },
   ];
   const state = {
@@ -41,32 +49,61 @@ test('should convert a list of user files to the file viewer structure', () => {
     name: 'test',
     children: [
       {
-        id: '1',
-        name: 'index1.js',
-        extension: 'js',
-      },
-      {
         id: expect.any(String),
         name: 'subfolder',
         children: [
+          {
+            id: expect.any(String),
+            name: 'another-subfolder',
+            children: [{ id: '1', name: 'index1.jsx', extension: 'jsx' }],
+          },
           {
             id: '2',
             name: 'index2.js',
             extension: 'js',
           },
+        ],
+      },
+      {
+        id: expect.any(String),
+        name: 'subfolder2',
+        children: [
           {
-            id: expect.any(String),
-            name: 'another-subfolder',
-            children: [{ id: '4', name: 'index4.js', extension: 'js' }],
+            id: '3',
+            name: 'index3.js',
+            extension: 'js',
           },
         ],
       },
       {
-        id: '3',
-        name: 'index3.js',
+        id: expect.any(String),
+        name: 'subfolder3',
+        children: [
+          {
+            id: expect.any(String),
+            name: 'another-subfolder',
+            children: [
+              {
+                id: '4',
+                name: 'index4.ts',
+                extension: 'ts',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: '5',
+        name: 'index5.js',
         extension: 'js',
+      },
+      {
+        id: '6',
+        name: 'index6.vue',
+        extension: 'vue',
       },
     ],
   };
+
   expect(selectFileViewerData(state)).toEqual(expectedResult);
 });
